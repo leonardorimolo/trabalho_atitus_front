58cd6389e86ef020c75b73a9bc96cb3b12cc65db
leonardorimolo <leonardorimolo1331@gmail.com>
2023-10-07 14:55:40 -0300

    Ajustes e correções.

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
+const header = document.querySelector('header');
+const headerHeight = header.offsetHeight;
+const corFundoPadrao = ''; 
 
+window.addEventListener('scroll', () => {
+  if (window.scrollY > headerHeight) {
+    header.style.backgroundColor = 'white'; 
+  } else {
+    header.style.backgroundColor = corFundoPadrao; 
+  }
+});
+
+getTamanhoTela = () => {
+  const tamanhoTela = window.innerHeight;
+  const pagina1 = document.querySelector('#first-page-blue-sky');
+  const pagina2 = document.querySelector('#second-page-yellow');
+
+  pagina1.style.height = tamanhoTela + 'px';
+  pagina2.style.height = tamanhoTela + 'px';
+}
+
+getTamanhoTela();
